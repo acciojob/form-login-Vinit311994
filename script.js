841564b34b7feac6cb6a3b7f5d3bb5014dd238ce
VINIT SINGH <130830294+Vinit311994@users.noreply.github.com>
2023-06-30 16:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	const firstName= document.getElementById("firstName").value;
+		const lastName= document.getElementById("firstName").value;
+	alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 
 }
+
+document.getElementById("loginForm").addEventListner("submit", function(event){
+event.preventDefault();
+	getFormValue();
+})
+
+
